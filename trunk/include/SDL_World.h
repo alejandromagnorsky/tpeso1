48e7334fc5db14fbf4cc9f255a2d9a64ac974f82
merchante.mariano <merchante.mariano@acecd291-e428-6978-ace0-7d7d7c5c1917>
2010-08-11 02:30:02 +0000

    Mapa que scrollea hecho, usen las flechas

--- trunk/include/SDL_World.h
+++ trunk/include/SDL_World.h
@@ -2,9 +2,9 @@
 #include "SDL_utils.h" 
 
 typedef struct {
-	int h,w;	//size
+	int h,w;		//size
+	int cameraX, cameraY;	//camera position
 	SDL_Surface * bg;
-
 } SDLWorld;
 
 
@@ -14,6 +14,8 @@ SDLWorld * getWorld(int h, int w, char * filename, char * ext);
 void endWorld(SDLWorld * world);	// libera recursos!! IMPORTANTE
 
 void renderSDLWorld(SDLWorld * world, SDL_Surface * screen);	// Por ahora, redibuja todo...
+
+void translateCamera(SDLWorld * world, int x, int y);
 
 
 // These update functions are used to update ants,traces and foods' positions
--- trunk/src/SDL_World.c
+++ trunk/src/SDL_World.c
@@ -5,9 +5,16 @@ SDLWorld * getWorld(int h, int w, char * filename, char * ext){
 	SDLWorld * out = malloc(sizeof(SDLWorld));
 	out->h = h;
 	out->w = w;
+	out->cameraX = 0;
+	out->cameraY = 0;
 	out->bg = loadImageSDL( filename, ext , 0);
 
 	return out;
+}
+
+void translateCamera(SDLWorld * world, int x, int y){
+	world->cameraX += x;
+	world->cameraY += y;
 }
 
 void endWorld(SDLWorld * world){
@@ -16,21 +23,30 @@ void endWorld(SDLWorld * world){
 }
 
 void renderGrid(SDLWorld * world, SDL_Surface * screen, int gridSize){
-	int i;
 
+	// Nota: tengo que saber la posicion de la camara, para desfasar todo.. es facil
+
+	int i;
+	int dX, dY; // camera position 
+
+	dX = world->cameraX % gridSize;
+	dY = world->cameraY % gridSize;
 	Uint32 gridColor = SDL_MapRGB( screen->format, 255, 255, 255 );
 
+	// Verticales
 	for(i=0;i<(screen->w / gridSize)+1;i++)
-		SDL_printLine(screen, i*gridSize, 0, i*gridSize, screen->h, gridColor );
+		SDL_printLine(screen, dX+ i*gridSize, 0, dX+ i*gridSize, screen->h, gridColor );
+
+	// Horizontales
 	for(i=0;i<(screen->h / gridSize)+1;i++)
-		SDL_printLine(screen, 0, i*gridSize, screen->w, i*gridSize, gridColor );
+		SDL_printLine(screen, 0, dY+i*gridSize, screen->w, dY+i*gridSize, gridColor );
 		
 }
 
 void renderSDLWorld(SDLWorld * world, SDL_Surface * screen){
 
 	blitSurface( screen, world->bg, 0, 0);
-	renderGrid(world,screen, 50);
+	renderGrid(world,screen, 40);
 
 }
 
