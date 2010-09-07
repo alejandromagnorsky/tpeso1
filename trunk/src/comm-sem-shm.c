589d2bf9b42f14b161d8da6f679bbccff6ed68c7
alejandromagnorsky <alejandromagnorsky@acecd291-e428-6978-ace0-7d7d7c5c1917>
2010-09-07 17:32:07 +0000

    Semaforos implementados con SV

--- trunk/src/comm-sem-shm.c
+++ trunk/src/comm-sem-shm.c
@@ -9,20 +9,24 @@
 #include <sys/msg.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <sys/sem.h>
 #include <semaphore.h>
-#include <errno.h>
 #include "../include/communication.h"
 
 #define SIZE sizeof(Message)
-#define semKey "/mutex"
 
+// Semaphores
+key_t keyRead = 1000;
+key_t keyWrite = 2000;
+int semRead;
+int semWrite;
 
-// Semaphore
-sem_t * sd;
 // For the shm
 int serverFd; 
 int clientFd;
+
 int clientquant;
+
 
 void 
 sigHandler(){
@@ -33,7 +37,8 @@ sigHandler(){
 
 void 
 destroyIPC(){
-	sem_unlink(semKey);
+	semctl(semRead, 0, IPC_RMID);
+	semctl(semWrite, 0, IPC_RMID);
 	shm_unlink("/server");
 	shm_unlink("/client");	
 }
@@ -41,54 +46,66 @@ destroyIPC(){
 
 void 
 openServer(void * t){
-	clientquant = (int)t+4;
-	if ( !(sd = sem_open(semKey, O_RDWR|O_CREAT, 0666, 1)) )	// Create and initialize the semaphore if isn't exists
-		errorLog("sem_open");
-	
+	int i;
+	clientquant = (int)t+3; // MODIFICAR
+		
+	if( (semRead = semget(keyRead, clientquant, 0666 | IPC_CREAT)) == -1) // Clientquant must take values between 500 and 2000
+		errorLog("semget");
+	if( (semWrite = semget(keyWrite, clientquant, 0666 | IPC_CREAT)) == -1) // Clientquant must take values between 500 and 2000
+		errorLog("semget");
+
+	for(i = 0; i < clientquant; i++)
+		semctl(semWrite, i, SETVAL, 1);
+
+
 	if ( (serverFd = shm_open("/server", O_RDWR|O_CREAT, 0666)) == -1 )
 		errorLog("sh_open");
-	ftruncate(serverFd, clientquant*SIZE);
+	ftruncate(serverFd, SIZE);
 
 	if ( (clientFd = shm_open("/client", O_RDWR|O_CREAT, 0666)) == -1 )
 		errorLog("sh_open");
 	ftruncate(clientFd, clientquant*SIZE);
 
 	Message * mem;
+	if ( !(mem = mmap(NULL, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, serverFd, 0)) )
+			errorLog("mmap");
+	memset(mem, 0, SIZE);
 	if ( !(mem = mmap(NULL, clientquant*SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, clientFd, 0)) )
 			errorLog("mmap");
 	memset(mem, 0, clientquant*SIZE);
-	if ( !(mem = mmap(NULL, clientquant*SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, serverFd, 0)) )
-			errorLog("mmap");
-	memset(mem, 0, clientquant*SIZE);
+	
 }
 
 
 
 void 
 openClient(void * t){
-	clientquant = (int)t+4;
-	if ( !(sd = sem_open(semKey, O_RDWR|O_CREAT, 0666, 1)) )	// Create and initialize the semaphore if isn't exists
-		errorLog("sem_open");
+	clientquant = (int)t+3; // MODIFICAR
 	
+	if( (semRead = semget(keyRead, clientquant, 0666 | IPC_CREAT)) == -1)
+		errorLog("semget");
+	if( (semWrite = semget(keyWrite, clientquant, 0666 | IPC_CREAT)) == -1)
+		errorLog("semget");
+
+
 	if ( (serverFd = shm_open("/server", O_RDWR|O_CREAT, 0666)) == -1 )
 		errorLog("sh_open");
-	ftruncate(serverFd, clientquant*SIZE);
+	ftruncate(serverFd, SIZE);
 
 	if ( (clientFd = shm_open("/client", O_RDWR|O_CREAT, 0666)) == -1 )
 		errorLog("sh_open");
 	ftruncate(clientFd, clientquant*SIZE);
 
 	Message * mem;
-	if ( !(mem = mmap(NULL, clientquant*SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, clientFd, 0)) )
+	if ( !(mem = mmap(NULL, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, serverFd, 0)) )
 			errorLog("mmap");
-	if ( !(mem = mmap(NULL, clientquant*SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, serverFd, 0)) )
+	if ( !(mem = mmap(NULL, clientquant*SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, clientFd, 0)) )
 			errorLog("mmap");
 }
 
 
 void
 closeIPC(){
-	sem_close(sd);
 	close(serverFd);
 	close(clientFd);
 }
@@ -100,45 +117,36 @@ receiveMessage(NodeType from, int key){
 	Message * mem;
 	Message * out = NULL;
 
-	int fd, i;
+	int index;
 
-	if(from == SERVER) // Ant case
-		fd = clientFd;
-	else				// Map case
-		fd = serverFd;
-
-	//printf("Recibiendo de FD: %d. Key: %d\n", fd, key);
-	if ( !(mem = mmap(NULL, clientquant*SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0)) )
-		errorLog("mmap");
-	
-	if(from == SERVER){ // The ant has to retrieve the message from an specific position in /client determinated by his key
-		mem += key;
-		sem_wait(sd);
-		// Block the condition in the while, the assigment of out and the readed flag
-		while(mem->keyTo != key){  // While the msg is read
-			sem_post(sd);
-		//	sleep(1);
-			sem_wait(sd);
-		}
-	} else { // The map has to search the /server to verify if any ant send a message
-		Message * aux;
-		aux = mem;
-				
-		sem_wait(sd);
-		for(i = 0; mem->keyTo != key; i++){ // While the msg is read
-			mem = aux + i%clientquant;
-			sem_post(sd);
-			//sleep(1);
-			//printf("Key: %d. Recibio mensaje? FD: %d. Index: %d. KeyFrom:%d KeyTo:%d \n", key,  fd, i%clientquant, mem->keyFrom, mem->keyTo);
-			sem_wait(sd);
-		}
+	if(from == SERVER) {	// Ant case
+		if ( !(mem = mmap(NULL, clientquant*SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, clientFd, 0)) )
+			errorLog("mmap");
+		index = key;
 	}
+	else {	// Map case
+		if ( !(mem = mmap(NULL, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, serverFd, 0)) )
+			errorLog("mmap");
+		index = 0;
+	}
+
+	mem += index;
+
+	struct sembuf semOpRead; // Operation for the write semaphore
+	semOpRead.sem_num = index; // Number of semaphore in the array
+	semOpRead.sem_op = -1;	
+
+	struct sembuf semOpWrite; // Operation for the write semaphore
+	semOpWrite.sem_num = index; // Number of semaphore in the array
+	semOpWrite.sem_op = 1;
+
+
+	semop(semRead, &semOpRead, 1);
 
 	out = createMessage(mem->keyFrom, mem->keyTo, mem->opCode,  mem->param, mem->pos, mem->trace);
 
-	mem->keyTo = 0; // Mark the message as readed
-	
-	sem_post(sd);
+	semop(semWrite, &semOpWrite, 1);
+
 
 	return out;
 }
@@ -147,34 +155,34 @@ int
 sendMessage(NodeType to, Message * msg){
 
 	Message * mem;	
-	int fd, index;
+	int index;
 	
-	if(to == SERVER)
-		fd = serverFd;	
-	else
-		fd = clientFd;
-//	printf("Key: %d enviando a FD: %d.\n", msg->keyFrom, fd);
-	
-	if ( !(mem = mmap(NULL, clientquant*SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0)) )
-		errorLog("mmap");
-
-	if(to == SERVER)
-		index = msg->keyFrom;
-	else
+	if(to == SERVER){
+		if ( !(mem = mmap(NULL, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, serverFd, 0)) )
+			errorLog("mmap");
+		index = 0;
+	}
+	else {
+		if ( !(mem = mmap(NULL, clientquant*SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, clientFd, 0)) )
+			errorLog("mmap");
 		index = msg->keyTo;
-
+	}
+	
 	mem += index;
 	
-	while(mem->keyTo != 0) // While the msg is not read	
-		printf("HOLAAAAAAAAAAAAAA\n");
-		
+	struct sembuf semOpWrite; // Operation for the write semaphore
+	semOpWrite.sem_num = index; // Number of semaphore in the array
+	semOpWrite.sem_op = -1;
 	
-	// Block this zone of code
-	sem_wait(sd);	
+	struct sembuf semOpRead; // Operation for the write semaphore
+	semOpRead.sem_num = index; // Number of semaphore in the array
+	semOpRead.sem_op = 1;
+
+	semop(semWrite, &semOpWrite, 1);
 
 	memcpy(mem, msg, SIZE);
 
-	sem_post(sd);
+	semop(semRead, &semOpRead, 1);
 
 	//printf("Mensaje mandado a %d. Enviado con keyTo = %d\n", index, mem->keyTo);
 
