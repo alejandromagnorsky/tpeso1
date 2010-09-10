278d27895beaaf13cb5bb8b0c7dd8afb9301c07a
alejandromagnorsky <alejandromagnorsky@acecd291-e428-6978-ace0-7d7d7c5c1917>
2010-09-10 21:44:20 +0000

    Ahora las hormigas agarran comida grande. Todavia no gritan

--- trunk/include/ant.h
+++ trunk/include/ant.h
@@ -8,17 +8,25 @@
 #include "../include/map.h"
 
 typedef enum { north, east, south, west, northwest, northeast, southeast, southwest } Cardinal;
+typedef enum {GET_FOOD, FOLLOW_TRACE, SET_SHOUT, FOLLOW_SHOUT, SMELL} AntOp;
 
 // If the variable Pos is called mov it refers to relative positions (up, down, left or right)
-// Else, the variable Pos is an absolut position
+// Else, the variable Pos is an absolute position
 
 typedef struct{
-	OpCode opCode;
+	AntOp op;
 	bool food;
 	Pos auxPos;
 	Pos anthill;
 	int key;
 }Ant;
+
+typedef struct{
+	int intensity; // If 1 the scream is no valid yet. If 0 the scream is valid. If -1 there is no scream
+	Pos pos;
+} Scream;
+
+extern Scream * screams;
 
 void * antMain(void * arg);
 
@@ -39,5 +47,13 @@ void setRegister(Ant * ant);
 bool getNearFood(Ant * ant, Pos to);
 
 bool setFood(Ant * ant, Pos to);
+
+void setShout(Ant * ant);
+
+bool followShout(Ant * ant);
+
+bool hasShouted(Ant * ant, Pos to);
+
+int getDistance(Pos from, Pos to);
 
 Pos getCurrentPos(int key);
