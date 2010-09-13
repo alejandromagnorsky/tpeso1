5c31edb0f64542c30256acb5713c84355d5af30b
legionariodeacero <legionariodeacero@acecd291-e428-6978-ace0-7d7d7c5c1917>
2010-09-13 18:25:25 +0000

    Frees en fifos y sockets

--- trunk/src/backend/IPC/comm-fifo.c
+++ trunk/src/backend/IPC/comm-fifo.c
@@ -15,7 +15,7 @@
 
 #define SRV_FIFO "/tmp/server"
 #define CLT_FIFO "/tmp/client_"
-#define CLT_KEY_BASE 3	// Map key: 1. Anthill key: 2. Clients keys are > 2
+#define CLT_KEY_BASE 2	// Map key: 1. Anthill key: 2. Clients keys are > 2
 
 int clientQuant = 0;
 
@@ -34,24 +34,15 @@ void sigHandler(){
 	exit(1);
 }
 
-// Close IPC resource
-void closeIPC(){
-	if ( close(serverRD) == -1 )
-		errorLog("Server's FIFO could not be closed.");
-	int i;
-	for (i=0; i<clientQuant; i++)
-		if ( close(clientsWR[i]) == -1 )
-			errorLog("A client's FIFO could not be closed.");
-}
-
 void closeServer(void * t, int size){
 	int i;
 	if ( close(serverRD) == -1 )
 		errorLog("Server's RDONLY FIFO could not be closed.");
 
-	for (i=CLT_KEY_BASE-1; i<clientQuant; i++)
+	for (i=0; i<clientQuant; i++)
 		if ( close(clientsWR[i]) == -1 )
 			errorLog("Clients' WRONLY FIFOs could not be closed.");
+	free(clientsWR);
 }
 
 void closeClient(void * t, int size){
@@ -59,9 +50,10 @@ void closeClient(void * t, int size){
 	if ( close(serverWR) == -1 )
 		errorLog("Server's WRONLY FIFO could not be closed.");
 
-	for (i=CLT_KEY_BASE-1; i<clientQuant; i++)
+	for (i=0; i<clientQuant; i++)
 		if ( close(clientsRD[i]) == -1 )
 			errorLog("Clients' RDONLY FIFOs could not be closed.");
+	free(clientsRD);
 }
 
 // Destroy IPC resources
@@ -74,11 +66,12 @@ void destroyIPC(){
                 errorLog("Server's FIFO could not be unlinked.");
 	if ( (fifoname = malloc(len)) == NULL )
 		errorLog("Memory allocation error in client's FIFO deletion.");
-	for (i=CLT_KEY_BASE-1; i<clientQuant; i++){
+	for (i=0; i<clientQuant; i++){
 		sprintf(fifoname, "%s%d", CLT_FIFO, i);
 		if ( unlink(fifoname) < 0 )
               		errorLog("Clients' FIFOs could not be unlinked.");
 	}
+	free(fifoname);
 }
 
 void createFifos(int clients){
@@ -91,11 +84,12 @@ void createFifos(int clients){
         if ( (fifoname = malloc(len)) == NULL )
                 errorLog("Memory allocation error in clients' FIFOs creation.");
 
-        for (i=CLT_KEY_BASE-1; i<clients; i++){
+        for (i=0; i<clients; i++){
                 sprintf(fifoname, "%s%d", CLT_FIFO, i);
                 if ( access(fifoname, 0) == -1 && mknod(fifoname, S_IFIFO | 0666, 0) == -1 )
                         errorLog("Clients' FIFOs could not be created.");
         }
+	free(fifoname);
 }
 
 void * openServerFifos(void * t){
@@ -104,24 +98,25 @@ void * openServerFifos(void * t){
 	char * fifoname;
 	clientsWR = malloc(clientQuant * sizeof(int));
 
-	if ( (fifoname = malloc(len)) == NULL )
+	if ( (fifoname = malloc(len)) == NULL  || clientsWR == NULL)
                 errorLog("Memory allocation error in server' FIFO opening.");
 
 	/* I'm SERVER and I open my FIFO for reading purposes only. */
         if ( (serverRD = open(SRV_FIFO, O_RDONLY)) == -1 )
                 errorLog("SERVER: Server's FIFO could not be opened.");
 
-        for (i=CLT_KEY_BASE-1; i<clientQuant; i++){
+        for (i=0; i<clientQuant; i++){
                 sprintf(fifoname, "%s%d", CLT_FIFO, i);
 		/* I'm SERVER and I open CLIENTs' FIFOS for writing purposes only. */
 		if ( (clientsWR[i] = open(fifoname, O_WRONLY)) == -1 )
 			errorLog("SERVER: Clients' FIFO could not be opened.");
         }
+	free(fifoname);
 	pthread_exit(NULL);
 }
 
 void openServer(void * t, int size){
-	clientQuant = (int) t + CLT_KEY_BASE;
+	clientQuant = (int) t + 1;	// Clients quantity (ants + anthill)
 
 	createFifos(clientQuant);
 
@@ -131,53 +126,44 @@ void openServer(void * t, int size){
 
 void openClient(void * t, int size){
 	int i;
-	clientQuant = (int) t + CLT_KEY_BASE;
+	clientQuant = (int) t + 1;	// Clients quantity (ants + anthill)
 	int len = strlen(CLT_FIFO) + digits(clientQuant-1);
 	char * fifoname;	
 	clientsRD = malloc(clientQuant * sizeof(int));
  	
 	createFifos(clientQuant);
 
-	if ( (fifoname = malloc(len)) == NULL )
+	if ( (fifoname = malloc(len)) == NULL || clientsRD == NULL )
                 errorLog("Memory allocation error in clients' FIFOs opening.");
 
 	/* I'm CLIENT and I open SERVER's FIFO for writing purposes only. */
         if ( (serverWR = open(SRV_FIFO, O_WRONLY)) == -1 )
                 errorLog("CLIENT: Server's FIFO could not be opened.");
 
-        for (i=CLT_KEY_BASE-1; i<clientQuant; i++){
+        for (i=0; i<clientQuant; i++){
                 sprintf(fifoname, "%s%d", CLT_FIFO, i);
 		/* I'm CLIENT and I open CLIENTS' FIFOS for reading purposes only. */
 		if ( (clientsRD[i] = open(fifoname, O_RDONLY)) == -1 )
 			errorLog("CLIENT: Clients' FIFO could not be opened.");
         }
+	free(fifoname);
 }
 
 int receiveFromClient(int key, char * buf, int size){
 	int bytes;
-	char * msg = malloc(size);
-
-	bytes = read(serverRD, msg, size);
-	memcpy(buf, msg, size);
-
-	free(msg);
+	bytes = read(serverRD, buf, size);
 	return bytes;
 }
 
 int receiveFromServer(int key, char * buf, int size){
 	int bytes;
-	char * msg = malloc(size);
-
-	bytes = read(clientsRD[key], msg, size);
-	memcpy(buf, msg, size);
-
-	free(msg);
+	bytes = read(clientsRD[key-CLT_KEY_BASE], buf, size);
 	return bytes;
 }
 
 int sendToClient(int key, char * buf, int size){
 	int bytes;
-	bytes = write(clientsWR[key], buf, size);
+	bytes = write(clientsWR[key-CLT_KEY_BASE], buf, size);
 
 	return bytes;
 }
